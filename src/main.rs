42c6b651108db79068dba412a57e16cedb851ec9
ayxan <ayxan.20145@gmail.com>
2026-09-07 11:12:56 +0400

    add clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    name = "tars",
+    version,
+    about = "a simple frontend for working with tar archives without having to remember complicated arguments"
+)]
+struct Cli {
+    command: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
